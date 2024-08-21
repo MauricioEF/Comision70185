78d5bb0cf64f74a481f9da16110ccd040b6d9fec
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2024-08-21 09:01:00 -0600

    Clase 9 finalizada

---
+++ clase9MotoresDePlantillas/src/routes/views.router.js
@@ -0,0 +1,44 @@
+import { Router } from "express";
+
+const viewsRouter = Router();
+const users = [
+    {
+        firstName:"Magali",
+        lastName:"Moyano"
+    },
+    {
+        firstName:"asda",
+        lastName:"Moya1231no"
+    },
+    {
+        firstName:"asdas",
+        lastName:"Mzxcxcno"
+    },
+    {
+        firstName:"M5757656ali",
+        lastName:"yty"
+    }
+]
+viewsRouter.get('/',(req,res)=>{
+    
+    res.render("Home",{
+        css:'home.css',
+        username: `Illeana`,
+        users
+    });
+})
+
+viewsRouter.get('/product',(req,res)=>{
+    const product = {
+        title:"producto feliz",
+        price:3000,
+        code:"AZXC8A",
+        offer:true
+    }
+    res.render("ProductDetails",{
+        css:'product.css',
+        product
+    });
+})
+
+export default viewsRouter;
