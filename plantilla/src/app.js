f73acc3073aedeb9efef897692f9caef0fae201f
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2024-08-25 20:02:44 -0600

    Nueva plantilla

--- plantilla/src/app.js
+++ plantilla/src/app.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import handlebars from 'express-handlebars'
+
+import __dirname from './utils.js';
+import viewsRouter from './routes/views.router.js';
 
 const app = express();
 
@@ -6,4 +10,12 @@ const PORT = process.env.PORT||8080;
 
 const server = app.listen(PORT,()=>console.log(`Listening on ${PORT}`));
 
+app.engine('handlebars',handlebars.engine());
+app.set('views',`${__dirname}/views`);
+app.set('view engine','handlebars');
+
+app.use(express.static(`${__dirname}/public`))
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/',viewsRouter);
